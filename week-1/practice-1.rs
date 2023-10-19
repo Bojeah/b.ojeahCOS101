464b2f2ec6da3163fb36ea63a6745f7ec3368913
b.ojeahCOS101 <benedictojeah@gmail.com>
2023-10-19 15:39:36 +0100

    (week 2 practice and projects)

--- week-1/practice-1.rs
+++ week-1/practice-1.rs
@@ -1,6 +1,6 @@
 fn main(){
-	println!("welcome to COS 101")
-	println!("The coruse learning outcome is as follows:")
+	println!("welcome to COS 101");
+	println!("The coruse learning outcome is as follows:");
 	println!("1. Distinguish between computer and computer programming,/n
 	          2. Develop some techniques in computer science,/n
 	          3. Understand the different areas of study in computer science,/n
