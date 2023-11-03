5851744160ccca1378458d7b39045b147ead2651
b.ojeahCOS101 <benedictojeah@gmail.com>
2023-11-03 12:49:34 +0100

    (week 4 project)

--- week-4/project_1/src/main.rs
+++ week-4/project_1/src/main.rs
@@ -1,24 +1,33 @@
 use std::io;
 
 fn main(){
+   loop{
 
    let mut d = String::new();
    let mut t = String::new();
 
 
-   println!("Enter your distance (in miles)");
+   println!("\nWhat is the distance travelled by the car (in miles)?");
    io::stdin().read_line(&mut d).expect("Not a valid string");
    let mut d:f32 = d.trim().parse().expect("Not a valid distance");
    d = d * 1.609344;
 
 
-   println!("Enter you time (in hours)");
+   println!("\nHow long did it take for the car to travel that distance (in hours)");
    io::stdin().read_line(&mut t).expect("Not a valid string");
    let t:f32 = t.trim().parse().expect("Not a valid time");
 
    let s:f32 = d / t;
 
-   println!("The speed of the f1 car is {}km/h", s);
+   println!("\nThe speed of the car is {}km/h", s);
+
+   let mut e = String::new();
+   println!("\nDo you want to continue. Y / N");
+   io::stdin().read_line(&mut e).expect("Not a valid string");
+   let e = e.trim();
+   if e == "N" {
+    break
+   }
 
 
 
@@ -26,4 +35,5 @@ fn main(){
 
 
 
+}
 }
