f769bda46d7d035a772626fcfb6ed24dc897babe
b.ojeahCOS101 <benedictojeah@gmail.com>
2024-09-10 13:19:58 +0100

    cos101

--- week-7/project-1/target/debug/deps/project_1.d
+++ week-7/project-1/target/debug/deps/project_1.d
@@ -1,5 +1,5 @@
-C:\Users\USER-PC\Documents\b.ojeahCOS101\week-7\project-1\target\debug\deps\project_1.exe: src\main.rs
+C:\Users\USER-PC\Documents\b.ojeahCOS101\week-7\project-1\target\debug\deps\project_1.exe: src/main.rs
 
-C:\Users\USER-PC\Documents\b.ojeahCOS101\week-7\project-1\target\debug\deps\project_1.d: src\main.rs
+C:\Users\USER-PC\Documents\b.ojeahCOS101\week-7\project-1\target\debug\deps\project_1.d: src/main.rs
 
-src\main.rs:
+src/main.rs:
